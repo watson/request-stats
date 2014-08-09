a3793d01dec4cce1d8b2d91674e0b31d5d747dad
Thomas Watson Steen <w@tson.dk>
2014-08-09 22:36:53 +0200

    Clean up tests

--- test.js
+++ test.js
@@ -60,111 +60,105 @@ var _respond = function (req, res) {
   req.resume();
 };
 
-describe('request-stats', function () {
-  afterEach(function () {
-    requestStats().removeAllListeners();
+describe('requestStats(server, onStats)', function () {
+  it('should call the stats-listener on request end', function (done) {
+    var server = http.createServer(_respond);
+    requestStats(server, function (stats) {
+      assert.statsFinished(stats);
+      done();
+    });
+    _start(server);
   });
 
-  describe('requestStats(server, onStats)', function () {
-    it('should call the stats-listener on request end', function (done) {
-      var server = http.createServer(_respond);
-      requestStats(server, function (stats) {
-        assert.statsFinished(stats);
-        done();
+  it('should call the stats-listener when the request is destroyed', function (done) {
+    var server = http.createServer(function (req, res) {
+      req.destroy();
+    });
+    requestStats(server, function (stats) {
+      assert.statsClosed(stats);
+    });
+    _start(server, function (err) {
+      assert(err instanceof Error);
+      done();
+    });
+  });
+
+  it('should calculate correct bytes read/written on keep-alive connections', function (done) {
+    var agent = new http.Agent();
+    agent.maxSockets = 1; // force connection reuse for every connection
+
+    var server = http.createServer(function (req, res) {
+      req.on('end', function () {
+        res.end();
       });
-      _start(server);
+      req.resume();
     });
 
-    it('should call the stats-listener when the request is destroyed', function (done) {
-      var server = http.createServer(function (req, res) {
-        req.destroy();
-      });
-      requestStats(server, function (stats) {
-        assert.statsClosed(stats);
-      });
-      _start(server, function (err) {
-        assert(err instanceof Error);
-        done();
-      });
-    });
-
-    it('should calculate correct bytes read/written on keep-alive connections', function (done) {
-      var agent = new http.Agent();
-      agent.maxSockets = 1; // force connection reuse for every connection
-
-      var server = http.createServer(function (req, res) {
-        req.on('end', function () {
-          res.end();
-        });
-        req.resume();
-      });
-
+    server.once('connection', function () {
       server.once('connection', function () {
-        server.once('connection', function () {
-          assert(false, 'Expected the TCP connection to be reused');
-        });
-      });
-
-      requestStats(server, function (stats) {
-        // assert req.bytes are around 1 million (we cannot know exactly since
-        // request headers will vary depending on the host system)
-        assert(stats.req.bytes > 1000000);
-        assert(stats.req.bytes < 1000300);
-      });
-
-      var performRequest = function (port, callback) {
-        var req = http.request({ port: port, method: 'PUT', agent: agent }, function (res) {
-          res.on('end', callback);
-          res.resume();
-        });
-        for (var n = 0, s = ''; n < 100000; n++) s += '1234567890'; // 1 million
-        req.end(s);
-      };
-
-      server.listen(0, function () {
-        var port = server.address().port;
-        performRequest(port, function () {
-          performRequest(port, function () {
-            server.close();
-            done();
-          });
-        });
+        assert(false, 'Expected the TCP connection to be reused');
       });
     });
-  });
 
-  describe('requestStats(req, res).once(...)', function () {
-    it('should call the stats-listener on request end', function (done) {
-      _start(http.createServer(function (req, res) {
-        requestStats(req, res).once('complete', function (stats) {
-          assert.statsFinished(stats);
+    requestStats(server, function (stats) {
+      // assert req.bytes are around 1 million (we cannot know exactly since
+      // request headers will vary depending on the host system)
+      assert(stats.req.bytes > 1000000);
+      assert(stats.req.bytes < 1000300);
+    });
+
+    var performRequest = function (port, callback) {
+      var req = http.request({ port: port, method: 'PUT', agent: agent }, function (res) {
+        res.on('end', callback);
+        res.resume();
+      });
+      for (var n = 0, s = ''; n < 100000; n++) s += '1234567890'; // 1 million
+      req.end(s);
+    };
+
+    server.listen(0, function () {
+      var port = server.address().port;
+      performRequest(port, function () {
+        performRequest(port, function () {
+          server.close();
           done();
         });
-        _respond(req, res);
-      }));
+      });
     });
   });
+});
 
-  describe('requestStats(server).once(...)', function () {
-    it('should call the stats-listener on request end', function (done) {
-      var server = http.createServer(_respond);
-      requestStats(server).once('complete', function (stats) {
+describe('requestStats(req, res).once(...)', function () {
+  it('should call the stats-listener on request end', function (done) {
+    _start(http.createServer(function (req, res) {
+      requestStats(req, res).once('complete', function (stats) {
         assert.statsFinished(stats);
         done();
       });
-      _start(server);
-    });
+      _respond(req, res);
+    }));
   });
+});
 
-  describe('requestStats(req, res, onStats)', function () {
-    it('should call the stats-listener on request end', function (done) {
-      _start(http.createServer(function (req, res) {
-        requestStats(req, res, function (stats) {
-          assert.statsFinished(stats);
-          done();
-        });
-        _respond(req, res);
-      }));
+describe('requestStats(server).once(...)', function () {
+  it('should call the stats-listener on request end', function (done) {
+    var server = http.createServer(_respond);
+    requestStats(server).once('complete', function (stats) {
+      assert.statsFinished(stats);
+      done();
     });
+    _start(server);
+  });
+});
+
+describe('requestStats(req, res, onStats)', function () {
+  it('should call the stats-listener on request end', function (done) {
+    _start(http.createServer(function (req, res) {
+      requestStats(req, res, function (stats) {
+        assert.statsFinished(stats);
+        done();
+      });
+      _respond(req, res);
+    }));
   });
 });
